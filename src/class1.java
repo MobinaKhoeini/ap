879794dcb8c1d60079ba38f7d0e8dc3331f46db2
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 00:20:07 +0330

    test scanner

--- src/class1.java
+++ src/class1.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("haha");
     }
 }
