8e581f70266fc2bca7b3139ee157071da55a1194
Mobina <mojtabakhoeini@gmail.com>
2025-02-28 23:45:53 +0330

    test Scanner

--- src/class1.java
+++ src/class1.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello worliii");
+        Scanner nana = new Scanner(System.in);
+        int n = nana.nextInt();
     }
 }
