551427d93c053be16ee29ca9c6457758ffb6281e
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 00:01:45 +0330

    Merge remote-tracking branch 'ap/master'
    # Conflicts:
    #	src/class1.java

--- src/class1.java
+++ src/class1.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner m = new Scanner(System.in);
+
     }
 }
