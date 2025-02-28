3d727739149017599c2726165badf1229cd6ad9b
MobinaKhoeini <mobina.khoeini12@gmail.com>
2025-02-28 22:42:26 +0330

    Update class1.java

--- src/class1.java
+++ src/class1.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class class1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("hello world");
     }
 }
