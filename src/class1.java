7b5b8746dd9f2940596b275ddf3a5ca9ddc39a86
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 00:01:45 +0330

    test scanner

--- src/class1.java
+++ src/class1.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner m = new Scanner(System.in);
+        int nana = m.nextInt();
+
     }
 }
