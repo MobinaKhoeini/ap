d8a12e28b411d7bb752ec4bedde9da816f319746
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 19:06:18 +0330

    exercise E.15

--- src/class1.java
+++ src/class1.java
@@ -2,28 +2,34 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("please enter the number:");
-        Scanner Num = new Scanner(System.in);
-        float Number = Num.nextFloat();
-        if (Number == 0)
+        System.out.println("please enter the income:");
+        Scanner income = new Scanner(System.in);
+        double salary = income.nextDouble();
+        double tax;
+        if (salary <= 50_000)
         {
-            System.out.println("zero");
+            tax = salary * 0.01;
         }
-        else if (Number > 0)
+        else if (salary > 50_000 && salary <= 75_000)
         {
-            System.out.println("positive");
+            tax = (50_000 * 0.01) + (salary * 0.02);
         }
-        else if (Number < 0)
+        else if (salary > 75_000 && salary <= 100_000)
         {
-            System.out.println("nagative");
+            tax = (50_000 * 0.01) + (75_000 * 0.02) + (salary * 0.03);
         }
-        if (Math.abs(Number) < 1)
+        else if (salary > 100_000 && salary <= 250_000)
         {
-            System.out.println("small");
+            tax = (50_000 * 0.01) + (75_000 * 0.02) + (100_000 * 0.03) + (salary * 0.04);
         }
-        else if (Math.abs(Number) > 1_000_000)
+        else if (salary > 250_000 && salary <= 500_000)
         {
-            System.out.println("large");
+            tax = (50_000 * 0.01) + (75_000 * 0.02) + (100_000 * 0.03) + (250_000 * 0.04) + (salary * 0.05);
         }
+        else
+        {
+            tax = (50_000 * 0.01) + (75_000 * 0.02) + (100_000 * 0.03) + (250_000 * 0.04) + (500_000 * 0.05) + (salary * 0.06);
+        }
+        System.out.println("your tax would be " + tax);
     }
 }
