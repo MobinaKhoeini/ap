ff5b97436cd0becbec139693ada612ac136f40f3
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 14:03:12 +0330

    exercise E.2

--- src/class1.java
+++ src/class1.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        Scanner n = new Scanner(System.in);
+        System.out.println("please enter the number:");
+        Scanner Num = new Scanner(System.in);
+        float Number = Num.nextFloat();
+        if (Number == 0)
+        {
+            System.out.println("zero");
+        }
+        else if (Number > 0)
+        {
+            System.out.println("positive");
+        }
+        else if (Number < 0)
+        {
+            System.out.println("nagative");
+        }
+        if (Math.abs(Number) < 1)
+        {
+            System.out.println("small");
+        }
+        else if (Math.abs(Number) > 1_000_000)
+        {
+            System.out.println("large");
+        }
     }
 }
