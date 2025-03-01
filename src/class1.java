8fb687b32a98e52e0adc1bb197858739288825b5
Mobina <mojtabakhoeini@gmail.com>
2025-03-01 21:33:01 +0330

    exercise E6.1 part e

--- src/class1.java
+++ src/class1.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        int Sum = 0;
-        for (int i = 2; i < 101 ; i++)
-        {
-            if(i % 2 == 0)
+        System.out.println("please enter your number:");
+        Scanner N = new Scanner(System.in);
+        int Number = N.nextInt();
+        int Remain = 0, Sum = 0;
+        while (Number > 0) {
+            Remain = Number % 10;
+            if (Remain % 2 != 0)
             {
-                Sum += i;
+                Sum +=Remain;
             }
+            Number = Number / 10;
         }
         System.out.println("the sum is " + Sum);
     }
