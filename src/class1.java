546c3cb66cbaabe91283565b36cf2ccf931bc4c5
Mobina <mojtabakhoeini@gmail.com>
2025-03-03 00:35:41 +0330

    exercise E6.13

--- src/class1.java
+++ src/class1.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-       Scanner Num = new Scanner(System.in);
-       int Number = Num.nextInt();
-       int Remain = 0;
-       while (Number > 0)
-       {
-           Remain = Number % 2;
-           Number = Number / 2;
-           System.out.println(Remain);
-       }
+        System.out.println("please enter a word:");
+       Scanner N = new Scanner(System.in);
+       String str = N.next();
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
 
     }
 }
