a992516dc213447e84874771841540c058a19e86
Mobina <mojtabakhoeini@gmail.com>
2025-03-03 18:37:44 +0330

    exercise E6.8

--- src/class1.java
+++ src/class1.java
@@ -1,3 +1,4 @@
+
 import sun.reflect.generics.reflectiveObjects.LazyReflectiveObjectGenerator;
 
 import java.util.Scanner;
@@ -5,8 +6,8 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
         System.out.println("please enter a word:");
-       Scanner N = new Scanner(System.in);
-       String str = N.next();
+        Scanner N = new Scanner(System.in);
+        String str = N.next();
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.charAt(i));
         }
