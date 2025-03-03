adac2f64b377ccd0acec4182faa75e740e2cf27f
Mobina <mojtabakhoeini@gmail.com>
2025-03-03 22:31:18 +0330

    exercise E6.2

--- src/class1.java
+++ src/class1.java
@@ -1,16 +1,76 @@
 
+import com.sun.xml.internal.ws.encoding.soap.SOAP12Constants;
 import sun.reflect.generics.reflectiveObjects.LazyReflectiveObjectGenerator;
 
+import javax.sound.midi.Sequence;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("please enter a word:");
-        Scanner N = new Scanner(System.in);
-        String str = N.next();
-        for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.charAt(i));
+        System.out.println("please input a sequence of integers:");
+       Scanner n = new Scanner(System.in);
+       int largest = n.nextInt();
+       int smallest = largest;
+       int Cumulative = largest;
+        List<Integer> adjDup = new ArrayList<>();
+        int preNum = largest;
+        int i = 0, j = 0;
+        if (largest % 2 == 0)
+        {
+            i++;
         }
+        else {
+            j++;
+        }
+        System.out.println("type done to continue");
+        System.out.println("cumulative totals:");
+        System.out.print(largest + " ");
+        while (n.hasNextInt())
+       {
+           //int currentNum = n.nextInt();
+           int Sequence = n.nextInt();
+           if (largest < Sequence)
+           {
+               largest = Sequence;;
+           }
+           if (smallest > Sequence)
+           {
+               smallest = Sequence;
+           }
+           if (Sequence % 2 == 0)
+           {
+               i++;
+           }
+           else
+           {
+               j++;
+           }
+           Cumulative += Sequence;
+           System.out.print( Cumulative + " ");
+           if (Sequence == preNum && !adjDup.contains(Sequence))
+           {
+               adjDup.add(Sequence);
+           }
+           preNum = Sequence;
+
+       }
+        System.out.println("the largest: " + largest);
+        System.out.println("the smallest: " + smallest);
+        System.out.println("the number of even inputs: " + i);
+        System.out.println("the number of odd inputs: " + j);
+        if (!adjDup.isEmpty()) {
+            System.out.print("adjacent duplicates:");
+
+            for (int num : adjDup) {
+                System.out.print(num + " ");
+            }
+        }
+        else System.out.println("no adjacent dupliates");
+
+
+
 
     }
 }
