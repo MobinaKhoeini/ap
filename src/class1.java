54549a74e280ba436fae88044b39b9095c9b0087
Mobina <mojtabakhoeini@gmail.com>
2025-03-04 19:26:03 +0330

    exercise E6.17

--- src/class1.java
+++ src/class1.java
@@ -11,14 +11,22 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
        Scanner S = new Scanner(System.in);
-       String[] str = new String[3];
-        for (int i = 0; i < 3; i++) {
-            System.out.println("please enter string " + (i+1) + ":");
-            str[i] = S.nextLine();
-        }
-        Arrays.sort(str);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(str[i]);
+       int n = S.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1  || j == 0  || j == n - 1)
+                {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print(" ");
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
 
 
