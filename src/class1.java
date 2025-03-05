ff2c9dc09bb3e4c44341e50b26095a1480d0e62b
Mobina <mojtabakhoeini@gmail.com>
2025-03-05 21:11:32 +0330

    exercise E6.9

--- src/class1.java
+++ src/class1.java
@@ -10,27 +10,13 @@ import java.util.Scanner;
 
 public class class1 {
     public static void main(String[] args) {
-        System.out.println("please enter the side length of the diamond:");
-        Scanner n = new Scanner(System.in);
-        int sLength = n.nextInt();
-        for (int i = 0 ; i < sLength; i++) {
-            for (int j = 0; j < sLength - i - 1 ; j++) {
-                System.out.print(" ");
+        System.out.println("please enter a word:");
+        Scanner S = new Scanner(System.in);
+        String str = S.next();
+        for (int i = str.length()-1 ; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+                }
+
             }
-            for (int j = 0; j < (2 * i + 1); j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int i = sLength - 2; i >= 0 ; i--) {
-            for (int j = 0; j <  sLength - i - 1; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j <= 2 * i ; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
 
-    }
-}
