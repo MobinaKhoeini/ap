5d10eb36d13249536ba23d90690b2006a837064a
Mobina <mojtabakhoeini@gmail.com>
2025-03-24 08:00:37 +0330

    EX2_PM_1_1 (correcting a mistake)

--- src/ap/exercises/ex2/Main_EX2_PM_1_1.java
+++ src/ap/exercises/ex2/Main_EX2_PM_1_1.java
@@ -6,19 +6,19 @@ public class Main_EX2_PM_1_1 {
     public static void main(String[] args) {
         System.out.println("please enter the size of the square:");
         Scanner s = new Scanner(System.in);
-        int Num = s.nextInt();
-        for (int i = 0; i < Num; i++) {
+        int k = s.nextInt();
+        for (int i = 0; i < k + 2; i++) {
             System.out.print("*");
         }
         System.out.println();
-        for (int i = 0; i < Num - 2; i++) {
+        for (int i = 0; i < k; i++) {
             System.out.print("*");
-            for (int j = 0; j < Num - 2; j++) {
+            for (int j = 0; j < k; j++) {
                 System.out.print(" ");
             }
             System.out.println("*");
         }
-        for (int i = 0; i < Num; i++) {
+        for (int i = 0; i < k + 2; i++) {
             System.out.print("*");
         }
     }
