81cbe35fd77abd8a531f3f0dc173173389d37ba4
Mobina <mojtabakhoeini@gmail.com>
2025-04-02 08:29:53 +0330

    Student class

---
+++ src/ap/exercises/ex2/Student.java
@@ -0,0 +1,14 @@
+package ap.exercises.ex2;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int studentNumber;
+    public String fieldOfStudy;
+    public Student(String firstName, String lastName, int studentNumber, String fieldOfStudy) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.fieldOfStudy = fieldOfStudy;
+    }
+}
