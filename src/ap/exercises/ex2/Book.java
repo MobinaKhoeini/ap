aa310cf341786323f1a1fc99519da41dde69cd94
Mobina <mojtabakhoeini@gmail.com>
2025-04-02 08:30:17 +0330

    Book class

---
+++ src/ap/exercises/ex2/Book.java
@@ -0,0 +1,14 @@
+package ap.exercises.ex2;
+
+public class Book {
+    public String name;
+    public String author;
+    public int pageNumber;
+    public int publicationYear;
+    public Book(String name, String author, int pageNumber, int publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.pageNumber = pageNumber;
+        this.publicationYear = publicationYear;
+    }
+}
