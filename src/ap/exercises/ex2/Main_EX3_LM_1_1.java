2299342feb7b53e889d47f3d6b80444bc084e01d
Mobina <mojtabakhoeini@gmail.com>
2025-04-02 08:27:12 +0330

    EX3_LM_1_1

---
+++ src/ap/exercises/ex2/Main_EX3_LM_1_1.java
@@ -0,0 +1,9 @@
+package ap.exercises.ex2;
+public class Main_EX3_LM_1_1 {
+    public static void main(String[] args) {
+        Book book1 = new Book("Pride and Prejudice", "Jane Austen", 259, 1813);
+        Book book2 = new Book("Pulp", "Charles Bukowski",208,1994);
+        Student student1 = new Student("Mahya", "Ahmadi", 404, "Chemistry engineering");
+        Student student2 = new Student("Ahmad", "hassani", 401, "Electrical engineering");
+    }
+}
