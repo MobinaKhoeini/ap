2b6845884829783a18bd93087ff2e775b7ed242c
Mobina <mojtabakhoeini@gmail.com>
2025-04-09 20:52:51 +0330

    EX3_LM_1_3

---
+++ src/ap/exercises/ex2/EX3_LM_1_3.java
@@ -0,0 +1,41 @@
+package ap.exercises.ex2;
+
+public class EX3_LM_1_3 {
+    public static class Student {
+        public String firstName;
+        public String lastName;
+        public int studentNumber;
+        public String fieldOfStudy;
+        public Student(String firstName, String lastName, int studentNumber, String fieldOfStudy) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.studentNumber = studentNumber;
+            this.fieldOfStudy = fieldOfStudy;
+        }
+        @Override
+        public String toString() {
+            return "Student {" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", studentNumber=" + studentNumber +
+                    ", fieldOfStudy='" + fieldOfStudy + '\'' +
+                    '}';
+        }
+    }
+    public static void main(String[] args) {
+        Student[] students = {
+                new Student("Sahar", "Hedayati", 401, "Mathematics"),
+                new Student("Mahya", "Ahmadi", 404, "Chemistry engineering"),
+                new Student("Ahmad", "hassani", 401, "Electrical engineering")
+        };
+        searchStudentByName(students, "sahar");
+
+    }
+    public static void searchStudentByName(Student[] students, String name) {
+        for (Student student : students) {
+            if (student.firstName.equalsIgnoreCase(name) || student.lastName.equalsIgnoreCase(name)) {
+                System.out.println(student);
+            }
+        }
+    }
+}
