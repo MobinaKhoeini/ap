793f8e226886f281598258eef3475bd1474624d4
Mobina <mojtabakhoeini@gmail.com>
2025-04-12 22:07:14 +0330

    Student class

--- src/ap/exercises/ex2/Student.java
+++ src/ap/exercises/ex2/Student.java
@@ -1,23 +1,23 @@
 package ap.exercises.ex2;
 
-public class Student {
-    public String firstName;
-    public String lastName;
-    public int studentNumber;
-    public String fieldOfStudy;
+class Student {
+    String firstName;
+    String lastName;
+    int studentNumber;
+    String fieldOfStudy;
+    Book[] borrowedBooks = new Book[5];
+    int borrowedCount = 0;
     public Student(String firstName, String lastName, int studentNumber, String fieldOfStudy) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.studentNumber = studentNumber;
         this.fieldOfStudy = fieldOfStudy;
     }
-    @Override
-    public String toString() {
-        return "Student{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", studentNumber=" + studentNumber +
-                ", fieldOfStudy='" + fieldOfStudy + '\'' +
-                '}';
+    void borrowBook(Book book) {
+        if (borrowedCount < borrowedBooks.length) {
+            borrowedBooks[borrowedCount++] = book;
+        }
     }
 }
+
+
