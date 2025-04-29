2376321ce2e76e73557be53b5bd58ff8207e8712
Mobina <mojtabakhoeini@gmail.com>
2025-04-29 22:48:46 +0330

    EX4_E3_6

---
+++ src/ap/exercises/ex3/EX3_6.java
@@ -0,0 +1,36 @@
+package ap.exercises.ex3;
+
+public class EX3_6 {
+    private int[] switchStates;
+    private int lampState;
+    public EX3_6() {
+        switchStates = new int[2];
+        switchStates[0] = 0;
+        switchStates[1] = 0;
+        lampState = 0;
+    }
+    public int getSwitchState(int switchNum) {
+        if (switchNum == 1 || switchNum == 2) {
+            return switchStates[switchNum - 1];
+        }
+        else {
+            throw new IllegalArgumentException("the number of switch can be 1 or 2");
+        }
+    }
+    public int getLampState() {
+        return lampState;
+    }
+    public void toggleSwitch(int switchNum) {
+        if (switchNum == 1 || switchNum == 2) {
+            switchStates[switchNum - 1] = 1 - switchStates[switchNum - 1];
+        }
+    }
+    private void updateLampState() {
+        if (switchStates[0] != switchStates[1]) {
+            lampState = 1;
+        }
+        else {
+            lampState = 0;
+        }
+    }
+}
