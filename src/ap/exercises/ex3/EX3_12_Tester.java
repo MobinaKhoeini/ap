fb63ceae6da9a163e220975bac17bb48a708a9d1
Mobina <mojtabakhoeini@gmail.com>
2025-04-29 23:31:35 +0330

    EX4_E3_12_tester

---
+++ src/ap/exercises/ex3/EX3_12_Tester.java
@@ -0,0 +1,12 @@
+package ap.exercises.ex3;
+
+public class EX3_12_Tester {
+    public static void main(String[] args) {
+        EX3_12 ex3_12 = new EX3_12("Mobina",10_000_000);
+        EX3_12 ex3_13 = new EX3_12("Hadis", 15_000_000);
+        System.out.println("name: " + ex3_12.getName());
+        System.out.println("salary: " + ex3_13.getSalary());
+        System.out.println("the salary would be: " + ex3_12.raisingSalary(20));
+        System.out.println("the salary is: " + ex3_13.raisingSalary(12));
+    }
+}
