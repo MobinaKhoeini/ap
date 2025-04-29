2b75497ac82efa8a4da43b28053f546b2165035e
Mobina <mojtabakhoeini@gmail.com>
2025-04-30 00:09:22 +0330

    EX4_E3_5

--- src/ap/exercises/ex4/EX3_5.java
+++ src/ap/exercises/ex4/EX3_5.java
@@ -10,18 +10,18 @@ public class EX3_5 {
     }
     public static void combinationTest(EX3_4 circuit, int firstSwitch, int secondSwitch) {
         setSwitchPositions(circuit, firstSwitch, secondSwitch);
-        int actual1 = circuit.getFirstSwitch();
-        int actual2 = circuit.getSecondSwitch();
+        int actualSwitch1 = circuit.getFirstSwitch();
+        int actualSwitch2 = circuit.getSecondSwitch();
         int actualLamp = circuit.getLampState();
         int expectedLamp = (firstSwitch != secondSwitch) ? 1 : 0;
         System.out.println("testing combinations:");
         System.out.println("first switch statement");
-        System.out.println("actual:" + actual1 + "\t" + "expected:" + firstSwitch);
+        System.out.println("actual:" + actualSwitch1 + "\t" + "expected:" + firstSwitch);
         System.out.println("second switch statement");
-        System.out.println("actual:" + actual2 + "\t" + "expected:" + secondSwitch);
+        System.out.println("actual:" + actualSwitch2 + "\t" + "expected:" + secondSwitch);
         System.out.println("lamp statement");
         System.out.println("actual:" + actualLamp + "\t" + "expected:" + expectedLamp);
-        if (actual1 == firstSwitch && actual2 == secondSwitch && actualLamp == expectedLamp) {
+        if (actualSwitch1 == firstSwitch && actualSwitch2 == secondSwitch && actualLamp == expectedLamp) {
             System.out.println("passed the test!");
         }
         else {
