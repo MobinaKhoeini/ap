afb3f209509a077e873f57b1bfbebda63fe9efc4
Mobina <mojtabakhoeini@gmail.com>
2025-04-29 23:22:28 +0330

    EX4_E3_15

---
+++ src/ap/exercises/ex3/EX3_15.java
@@ -0,0 +1,23 @@
+package ap.exercises.ex3;
+
+public class EX3_15 {
+    private String sender;
+    private String recipient;
+    private String body;
+    public EX3_15(String sender, String recipient) {
+        this.sender = sender;
+        this.recipient = recipient;
+        body = "";
+    }
+    public void addingLine(String line) {
+        body = body.concat(line).concat("\n");
+        //body += line + "\n";
+    }
+    public String getLetter() {
+        String letter = "Dear " + recipient + ":\n\n";
+        letter = letter.concat(body);
+        letter = letter.concat("\nSincerely,\n\n");
+        letter = letter.concat(sender);
+        return letter;
+    }
+}
