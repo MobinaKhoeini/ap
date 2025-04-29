8d3c2bc87a66203237373fe92f1d6d93bfe65dd3
Mobina <mojtabakhoeini@gmail.com>
2025-04-29 23:22:43 +0330

    EX4_E3_15_Tester

---
+++ src/ap/exercises/ex3/EX3_15_Tester.java
@@ -0,0 +1,9 @@
+package ap.exercises.ex3;
+
+public class EX3_15_Tester {
+    public static void main(String[] args) {
+        EX3_15 ex3_15 = new EX3_15("Mobina","hadis");
+        ex3_15.addingLine("happay birthday! hope this year can change your perspective and make you happier.");
+        System.out.println(ex3_15.getLetter());
+    }
+}
